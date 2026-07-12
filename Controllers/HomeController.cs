e49141af373d4f3f013cec8f38fa0d639b680e69
Meryem Güçlü <lifevern@gmail.com>
2026-07-13 00:11:32 +0300

    Revert "Removed database dependency to simplify deployment"
    This reverts commit 9cf23281b5069b589d32c14a73e1069c6fb200ae.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,15 +7,29 @@ namespace PortfolioWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(ContactMessage model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.ContactMessages.Add(model);
+                await _context.SaveChangesAsync();
+                return Json(new { success = true, message = "Mesajınız başarıyla gönderildi!" });
+            }
+            return Json(new { success = false, message = "Lütfen form alanlarını kontrol edin." });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
